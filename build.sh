d2dbace9a7e563e02b81b4da9fa5ee8cbc1c920e
Damien Seguy <damien.seguy@gmail.com>
2017-02-20 19:31:45 +0100

    Build script

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
+#Building
 docker build --no-cache -t exakat/exakat:0.10.2 .
+
+#Check for installation
+docker run -it --rm exakat/exakat:latest doctor
+
+#Running Exakat
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:latest init -p sculpin -R https://github.com/sculpin/sculpin.git -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:latest project -p sculpin -v
+
+
