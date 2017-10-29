b1e5ae5c74ccbcc8c6a966f9867cab7b86ea756f
Damien Seguy <damien.seguy@gmail.com>
2017-10-29 12:02:35 -0400

    Added examples for svn / hg

--- build.sh
+++ build.sh
@@ -5,7 +5,19 @@ docker build --no-cache -t exakat/exakat:1.0.0 .
 docker run -it --rm exakat/exakat:1.0.0 doctor
 
 #Running Exakat
+rm -rf projects/sculpin
 docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.0.0 init -p sculpin -R https://github.com/sculpin/sculpin.git -v
 docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.0.0 project -p sculpin -v
+
+
+rm -rf projects/ice
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.0.0 init -p ice -R https://bitbucket.org/ifacesoft/ice -hg -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.0.0 project -p ice -v
+
+rm -rf projects/fry
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.0.0 init -p fry -R https://svn.code.sf.net/p/fry/code -svn -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.0.0 project -p fry -v
+
+
 
 
