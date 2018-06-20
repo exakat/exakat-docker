b17cb988cefea5bdda5d174eb3f074a84da0ba9d
Damien Seguy <damien.seguy@gmail.com>
2018-06-20 08:27:00 +0200

    Bumped versions

--- build.sh
+++ build.sh
@@ -1,23 +1,33 @@
 #Building
-docker build --no-cache -t exakat/exakat:1.1.8 .
+docker build --no-cache -t exakat/exakat:1.3.2 .
 
 #Check for installation
-docker run -it --rm exakat/exakat:1.1.8 doctor
+docker run -it --rm exakat/exakat:1.3.2 doctor
 
 #Running Exakat
 rm -rf projects/sculpin
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.1.8 init -p sculpin -R https://github.com/sculpin/sculpin.git -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.1.8 project -p sculpin -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 init -p sculpin -R https://github.com/sculpin/sculpin.git -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 project -p sculpin -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 report -p sculpin -format Ambassador 
 
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 init -p owncloud -R https://github.com/owncloud/core.git -v
+docker run -it --rm -e JAVA_OPTIONS="-Xms32m -Xmx2512m" -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 project -p owncloud -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 report -p owncloud -format Ambassador 
+
+
+
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 codacy -p codacy 
 
 rm -rf projects/ice
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.1.8 init -p ice -R https://bitbucket.org/ifacesoft/ice -hg -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.1.8 project -p ice -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 init -p ice -R https://bitbucket.org/ifacesoft/ice -hg -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 project -p ice -v
 
 rm -rf projects/fry
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.1.8 init -p fry -R https://svn.code.sf.net/p/fry/code -svn -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.1.8 project -p fry -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 init -p fry -R https://svn.code.sf.net/p/fry/code -svn -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.3.2 project -p fry -v
 
 
+#Interactive
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bin/bash exakat/exakat:1.3.2
 
 
