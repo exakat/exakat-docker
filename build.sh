9f432622501e0761b9e626302efa5ad79acc04f4
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-07-20 11:13:21 +0100

    Extract build from circle.yml

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # Build exakat docker
-docker build --no-cache -t exakat/codacy .
+docker build -f gremlin.dockerfile -t exakat/gremlin:latest .
+docker run --rm -it -v "$PWD:/mnt" exakat/gremlin:latest
+docker build --no-cache -t codacy/codacy-exakat:latest .
