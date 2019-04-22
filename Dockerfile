62e606f69b1687102d1fe7cc419cd883a8758fc1
Damien Seguy <damien.seguy@gmail.com>
2019-04-22 19:02:14 +0200

    Bumped to version 1.7.6

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM php:7.3-cli-stretch
 
 LABEL MAINTAINER Exakat, Damien Seguy, dseguy@exakat.io
-ENV EXAKAT_VERSION 1.7.1
-ENV GREMLIN_VERSION 3.3.5
+ENV EXAKAT_VERSION 1.7.5
+ENV GREMLIN_VERSION 3.3.6
 
 COPY exakat.sh /usr/src/exakat/
 COPY config/exakat.ini /usr/src/exakat/config/
--- build.sh
+++ build.sh
@@ -1,33 +1,50 @@
 #Building
-docker build --no-cache -t exakat/exakat:1.5.7 .
+docker build --no-cache -t exakat/exakat:1.7.6 .
+
+docker build --no-cache -t exakat/exakat-alpine:1.7.0 --file Dockerfile-alpine . 
+
+docker build -t exakat/exakat:1.6.6 .
 
 #Check for installation
-docker run -it --rm exakat/exakat:1.5.7 doctor
+docker run -it --rm exakat/exakat:1.6.6 doctor
 
 #Running Exakat
 rm -rf projects/sculpin
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 init -p sculpin -R https://github.com/sculpin/sculpin.git -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 project -p sculpin -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 report -p sculpin -format Ambassador 
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.7.0 init -p sculpin -R https://github.com/sculpin/sculpin.git -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.7.0 project -p sculpin -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat-alpine:1.7.0 project -p sculpin -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.7.0 report -p sculpin -format Ambassador 
 
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 init -p owncloud -R https://github.com/owncloud/core.git -v
-docker run -it --rm -e JAVA_OPTIONS="-Xms32m -Xmx2512m" -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 project -p owncloud -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 report -p owncloud -format Ambassador 
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 init -p owncloud -R https://github.com/owncloud/core.git -v
+docker run -it --rm -e JAVA_OPTIONS="-Xms32m -Xmx2512m" -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 project -p owncloud -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 report -p owncloud -format Ambassador 
 
 
 
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 codacy -p codacy 
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 codacy -p codacy 
 
 rm -rf projects/ice
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 init -p ice -R https://bitbucket.org/ifacesoft/ice -hg -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 project -p ice -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 init -p ice -R https://bitbucket.org/ifacesoft/ice -hg -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 project -p ice -v
 
 rm -rf projects/fry
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 init -p fry -R https://svn.code.sf.net/p/fry/code -svn -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.5.7 project -p fry -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 init -p fry -R https://svn.code.sf.net/p/fry/code -svn -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 project -p fry -v
 
 
 #Interactive
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bin/bash exakat/exakat:1.5.7
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bin/bash exakat/exakat:1.7.0
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bin/bash exakat/base:3.3.4 
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bin/bash 3aed948e371b
+
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bin/sh 79ba210fca70
 
 
+
+
+
+docker build --no-cache -t exakat/base:3.3.4 .
+
+
+docker run -it --rm -v /Users/famille/Desktop/analyzeG3/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 project -p wstmall -v
+docker run -it --rm -v /Users/famille/Desktop/analyzeG3/projects:/usr/src/exakat/projects exakat/exakat-alpine:1.6.6 project -p wstmall -v
