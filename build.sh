61daee6b0c87086e88cce6fb44186d57e918ef7f
Damien Seguy <damien.seguy@gmail.com>
2019-05-28 17:31:05 +0200

    Bump to version 1.8.1

--- build.sh
+++ build.sh
@@ -1,23 +1,26 @@
 #Building
-docker build --no-cache -t exakat/exakat:1.7.7 .
+docker build --no-cache -t exakat/exakat:1.8.0-local .
+docker images pull exakat/exakat:1.8.0 .
+
+docker build --no-cache -t exakat/exakat:gitlab  --file Dockerfile-gitlab . 
 
 docker build --no-cache -t exakat/base:3.3.6 .
 
 docker build --no-cache -t exakat/gitlab  --file Dockerfile-gitlab . 
-docker run -it --rm  --entrypoint /bin/sh exakat/gitlab
+docker run -it --rm  --entrypoint /bin/sh exakat/exakat:1.7.8-local
+docker run -it --rm  --entrypoint /bin/sh exakat/exakat:latest
 
 
 docker build --no-cache -t exakat/exakat-alpine:1.7.0 --file Dockerfile-alpine . 
 
-docker build -t exakat/exakat:1.6.6 .
-
 #Check for installation
-docker run -it --rm exakat/exakat:1.6.6 doctor
+docker run -it --rm exakat/exakat:1.7.8-local doctor
+docker run -it --rm exakat/exakat:latest doctor
 
 #Running Exakat
 rm -rf projects/sculpin
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.7.0 init -p sculpin -R https://github.com/sculpin/sculpin.git -v
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.7.0 project -p sculpin -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:latest init -p sculpin -R https://github.com/sculpin/sculpin.git -v
+docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:latest project -p sculpin -v
 docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat-alpine:1.7.0 project -p sculpin -v
 docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.7.0 report -p sculpin -format Ambassador 
 
@@ -26,8 +29,6 @@ docker run -it --rm -e JAVA_OPTIONS="-Xms32m -Xmx2512m" -v $(pwd)/projects:/usr/
 docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 report -p owncloud -format Ambassador 
 
 
-
-docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 codacy -p codacy 
 
 rm -rf projects/ice
 docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 init -p ice -R https://bitbucket.org/ifacesoft/ice -hg -v
@@ -46,6 +47,7 @@ docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bi
 docker run -it --rm -v $(pwd)/projects:/usr/src/exakat/projects --entrypoint /bin/sh 79ba210fca70
 
 
+docker run -it --rm -w /src -v $(pwd)/projects/myself/code:/src --entrypoint "/usr/src/exakat/exakat.phar" exakat/exakat:latest  project -v
 
 
 
@@ -54,3 +56,15 @@ docker build --no-cache -t exakat/base:3.3.4 .
 
 docker run -it --rm -v /Users/famille/Desktop/analyzeG3/projects:/usr/src/exakat/projects exakat/exakat:1.6.6 project -p wstmall -v
 docker run -it --rm -v /Users/famille/Desktop/analyzeG3/projects:/usr/src/exakat/projects exakat/exakat-alpine:1.6.6 project -p wstmall -v
+
+docker run -it --rm  --entrypoint /bin/sh sixlive/php-lint-fix
+docker run -it --rm  --entrypoint /bin/sh exakat/exakat:gitlab
+
+
+docker run -it --rm -w /src -v $(pwd)/projects/backbee/code:/src --entrypoint "/usr/src/exakat/exakat.phar" exakat/exakat:latest  project -v
+
+docker run -it --rm -w /src -v $(pwd)/projects/backbee/code:/src exakat/exakat:latest project -v
+docker run -it --rm -v $(pwd)/projects/backbee/code:/src exakat/exakat:latest project -v
+docker run -it --rm -v $(pwd)/projects/backbee/code:/src exakat/exakat:1.7.8-local project -v
+
+/usr/src/exakat/exakat.phar project -v
